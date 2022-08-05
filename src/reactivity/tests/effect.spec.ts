e9be26dddc74bc3434daec44f18b881a72fc9c45
zhaoce <cehzhao0206@163.com>
2022-08-05 08:35:59 +0800

    effect 的 scheduler 功能单测

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -30,4 +30,34 @@ describe("effect", () => {
         expect(count).toBe(12);
         expect(r).toBe("count");
     });
+
+    it("scheduler", () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            {
+                scheduler,
+            },
+        );
+
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        // should be called on first trigger
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // should not run yet
+        expect(dummy).toBe(1);
+        // manually run
+        run();
+        // should han run
+        expect(dummy).toBe(2);
+    });
 });
