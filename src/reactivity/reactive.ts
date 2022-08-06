ed39812db72f7b941462ec08a155aed3b04b789a
zhaoce <cehzhao0206@163.com>
2022-08-07 00:18:19 +0800

    实现 isReactive 和 isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,11 +1,24 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 
+export const enum ReactiveFlags {
+    IS_REACTIVE = "_v_isReactive",
+    IS_READONLY = "_v_isReadonly",
+}
+
 export function reactive(obj: any): any {
     return createActiveObj(obj, mutableHandlers);
 }
 
 export function readonly(obj: any): any {
     return createActiveObj(obj, readonlyHandlers);
+}
+
+export function isReactive(value: any): boolean {
+    return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value: any): boolean {
+    return !!value[ReactiveFlags.IS_READONLY];
 }
 
 function createActiveObj(obj: any, baseHandlers: any) {
