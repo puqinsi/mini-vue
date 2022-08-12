ec10261a5c904fa604791a0bac233998321d4441
zhaoce <cehzhao0206@163.com>
2022-08-12 22:17:07 +0800

    实现 isRef 和 unRef 功能

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref, unRef } from "../ref";
 
 describe("ref", () => {
     it("happy path", () => {
@@ -43,5 +44,21 @@ describe("ref", () => {
         a.value.count++;
         expect(a.value.count).toBe(2);
         expect(dummy).toBe(2);
+    });
+
+    it("isRef", () => {
+        const a = ref(1);
+        const person = reactive({
+            age: 10,
+        });
+        expect(isRef(a)).toBe(true);
+        expect(isRef(1)).toBe(false);
+        expect(isRef(person)).toBe(false);
+    });
+
+    it("unRef", () => {
+        const a = ref(1);
+        expect(unRef(a)).toBe(1);
+        expect(unRef(1)).toBe(1);
     });
 });
