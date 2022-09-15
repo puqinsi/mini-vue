371abe43f7d35f0932f9046cb83faebf0bbdecb0
朴勤思 <30425020+puqinsi@users.noreply.github.com>
2022-09-15 16:16:09 +0800

    Revert "Dev (#24)" (#25)
    This reverts commit a2858bdd95bceb418867208bf093d164f1a85ff2.

--- src/runtime-core/index.ts
+++ src/runtime-core/index.ts
@@ -3,4 +3,3 @@ export { h } from "./h";
 export { renderSlots } from "./helpers/renderSlots";
 export { createTextVNode } from "./vnode";
 export { getCurrentInstance } from "./component";
-export { provide, inject } from "./apiInject";
